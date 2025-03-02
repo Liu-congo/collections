a5ebe846a8202406f930be43cc1ddbde5e0c3657
Liu-congo <1502632128@qq.com>
2025-03-02 01:25:05 -0800

    add discussion1 in cs61b chapter1

--- cs61b/chapter1.py
+++ cs61b/chapter1.py
@@ -79,3 +79,73 @@ class breakCotinue:
     
 tmp = breakCotinue()
 tmp.main()
+
+# Discussion 1.1
+size = int(27)
+name = "Fido"
+class Dog:
+    def __init__(self, name:str, size:int):
+        self.name = name
+        self.size = size 
+        pass
+
+    def bark(self, times:int):
+        """
+        :param times: the number of barking times
+        """
+        for i in range(times):
+            print("Barking...")
+        return
+    
+    def play(self):
+        print("playing...")
+        return
+    
+myDog = Dog(name,size)
+x = size - 5
+if x < 15:
+    myDog.bark(8)
+while x > 3:
+    x -= 1
+    myDog.play()
+numList = [2,4,6,8]
+print("Hello ")
+print("Dog: " + name)
+
+print(numList[1])
+if numList[3] == 8:
+    print("potato")
+
+
+# Discussion 1.2
+def mystery(inputArray: list, k: int) -> int:
+    x = inputArray[k]
+    answer = k
+    index = k + 1
+    while index < len(inputArray):
+        if inputArray[index] < x:
+            x = inputArray[index]
+            answer = index
+        index = index + 1
+    return answer
+print(mystery([3, 0, 4, 6, 3], 2))
+
+def mystery2(inputArray: list) -> None:
+    index = 0
+    while index < len(inputArray):
+        targetIndex = mystery(inputArray, index)
+        temp = inputArray[targetIndex]
+        inputArray[targetIndex] = inputArray[index]
+        inputArray[index] = temp
+        index = index + 1
+    return inputArray
+print(mystery2([3, 0, 4, 6, 3]))
+
+# Discussion 1.3
+def fib(n: int) -> int:
+    assert n >= 1, "n should be no smaller than 1"
+    fib_list = [0,1]
+    while len(fib_list) < n+1:
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib_list[n-1]
+print(fib(5))
